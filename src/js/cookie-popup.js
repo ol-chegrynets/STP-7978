cc5756b9eacd6b968a49e036a8a1846555c4fab1
OksanaLynnyk <allimpiya@gmail.com>
2025-02-24 22:36:07 +0200

    Fix header and cookie

--- src/js/cookie-popup.js
+++ src/js/cookie-popup.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const popup = document.querySelector(".cookie-popup");
-    const overlay = document.querySelector(".overlay");
-  
+  const popup = document.querySelector(".cookie-popup");
+  const overlay = document.querySelector(".overlay");
+
+  const cookieStatus = localStorage.getItem("cookieConsent");
+
+  if (!cookieStatus) {
     setTimeout(() => {
       popup.classList.add("show-popup");
       overlay.classList.add("show-overlay");
     }, 5000);
-  
-    document.querySelectorAll(".decline-btn, .accept-btn").forEach((button) => {
-      button.addEventListener("click", () => {
-        popup.classList.remove("show-popup");
-        overlay.classList.remove("show-overlay");
-      });
-    });
+  }
+
+  document.querySelector(".accept-btn").addEventListener("click", () => {
+    localStorage.setItem("cookieConsent", "accepted");
+    closePopup();
   });
+
+  document.querySelector(".decline-btn").addEventListener("click", () => {
+    localStorage.setItem("cookieConsent", "declined");
+    closePopup();
+  });
+
+  function closePopup() {
+    popup.classList.remove("show-popup");
+    overlay.classList.remove("show-overlay");
+  }
+});
